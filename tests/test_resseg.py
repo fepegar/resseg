1dfb21e3b2c71d80ce516beeae362bf77aa540d1
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-02-04 12:12:19 +0000

    Add empty test

--- tests/test_resseg.py
+++ tests/test_resseg.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `resseg` package."""
+# TODO
 
 
 import unittest
 from click.testing import CliRunner
-
-from resseg import resseg
-from resseg import cli
 
 
 class TestResseg(unittest.TestCase):
@@ -25,10 +23,3 @@ class TestResseg(unittest.TestCase):
 
     def test_command_line_interface(self):
         """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'resseg.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
