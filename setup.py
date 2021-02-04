a058b39bb16bba5af80a54a856b677ae79be5ef1
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-02-04 12:08:48 +0000

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     packages=find_packages(include=['resseg', 'resseg.*']),
     test_suite='tests',
     url='https://github.com/fepegar/resseg',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
 )
