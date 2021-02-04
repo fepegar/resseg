eccfde10266830961987be3d3ca3a2738f66b016
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-02-04 17:16:28 +0000

    Update hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,8 @@
-dependencies = ['torch', 'torchvision', 'unet', 'torchio']
+dependencies = ['torch', 'torchvision', 'unet']
 
 
-from resseg.model import ressegnet
+import sys
+from pathlib import Path
+code_dir = Path(__file__).parent / 'resseg'
+sys.path.insert(0, str(code_dir))
+from model import ressegnet
