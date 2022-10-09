fbf15115c1804316a081e3c3d1624de29ef1f6cb
Fernando Pérez-García <fepegar@gmail.com>
2022-10-09 20:03:14 +0100

    Use top-level variables for fix arguments

--- resseg/transform.py
+++ resseg/transform.py
@@ -6,19 +6,22 @@ from constants import IMAGE_NAME
 from constants import TO_MNI
 
 
+RESSEG_LANDMARKS = np.array([
+      0.        ,   0.31331614,   0.61505419,   0.76732501,
+      0.98887953,   1.71169384,   3.21741126,  13.06931455,
+     32.70817796,  40.87807389,  47.83508873,  63.4408591 ,
+    100.
+])
+NUM_LEVELS_UNET = 3
+
+
 def get_preprocessing_transform(
     input_path,
     mni_transform_path=None,
     interpolation='bspline',
     tolerance=0.1,
 ):
-    landmarks = np.array([
-            0.        ,   0.31331614,   0.61505419,   0.76732501,
-            0.98887953,   1.71169384,   3.21741126,  13.06931455,
-           32.70817796,  40.87807389,  47.83508873,  63.4408591 ,
-          100.
-    ])
-    hist_std = tio.HistogramStandardization({IMAGE_NAME: landmarks})
+    hist_std = tio.HistogramStandardization({IMAGE_NAME: RESSEG_LANDMARKS})
     preprocess_transforms = [
         tio.ToCanonical(),
         hist_std,
@@ -34,6 +37,8 @@ def get_preprocessing_transform(
             kwargs['target'] = tio.datasets.Colin27().t1.path
         resample_transform = tio.Resample(**kwargs)
         preprocess_transforms.append(resample_transform)
-    preprocess_transforms.append(tio.EnsureShapeMultiple(8, method='crop'))
+    target = int(2 ** NUM_LEVELS_UNET)
+    ensure_shape = tio.EnsureShapeMultiple(target, method='crop')
+    preprocess_transforms.append(ensure_shape)
     preprocess_transform = tio.Compose(preprocess_transforms)
     return preprocess_transform
